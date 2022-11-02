ce03ea0a6a2cce989c7740a6c24458af142d9b5c
N <adamsnoah999@gmail.com>
2022-11-01 21:05:04 -0500

    Worked on game state

---
+++ Assets/AdamsNoah_Game/Script/TurnGameSM.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnGameSM : StateMachine
+{
+    private void Start()
+    {
+        // set starting State here
+    }
+}
