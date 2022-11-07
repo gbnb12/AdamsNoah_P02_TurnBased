7e75e9253a6967c9d759dec2a84a19a488841c5b
N <adamsnoah999@gmail.com>
2022-11-06 22:53:17 -0600

    Set up actions, attack, laser, heal

--- Assets/TurnGame/Script/Health.cs
+++ Assets/TurnGame/Script/Health.cs
@@ -1,8 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : MonoBehaviour, IDamageable
 {
-    public int __currentHealth = 3;
+    public int _currentHealth = 100;
+
+    [SerializeField] Text _healthText;
+
+    void Start()
+    {
+        _currentHealth = 100;
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _currentHealth -= amount;
+        _healthText.GetComponent<Text>().text = "Health: " + _currentHealth;
+        Debug.Log("Game Piece took damage: " + amount);
+        if(_currentHealth <= 0)
+        {
+            Kill();
+        }
+    }
+
+    public void Heal(int level)
+    {
+        _currentHealth += level;
+    }
 }
